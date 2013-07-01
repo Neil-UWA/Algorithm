58742438fe4f8a08b090a8ac85c5f879aabd88d3
Neil <neil-lu@graduate.uwa.edu.au>
2013-07-01 16:36:42 +0800

    reformated the code to make it pretty

--- Sorting/bubble.c
+++ Sorting/bubble.c
@@ -4,6 +4,7 @@ void bubble_sort_v0(int *in, int size) {
 	//int	swap_count = 0;
 
 	printf("\nbubble_sort_v0 \n");
+
 	for (int i = 0; i < size - 1; i++) {
 		for (int j = 0; j < size-1; j++) {
 			if (is_greater(in[j], in[j+1])) {
@@ -18,14 +19,17 @@ void bubble_sort_v0(int *in, int size) {
 
 void bubble_sort_v1(int *in, int size) {
 	printf("\nbubble_sort_v1 \n");
+
 	for (int i = 0; i < size - 1; i++) {
 		bool swap_flag = false;
+
 		for (int j = 0; j < size-1; j++) {
 			if (is_greater(in[j], in[j+1])) {
 				swap(&in[j], &in[j+1]);
 				swap_flag	=	true;
 			}
 		}
+
 		if (false == swap_flag) {
 			break;
 		}
@@ -36,17 +40,21 @@ void bubble_sort_v2(int *in, int size) {
 	int input_size = size;
 
 	printf("\nbubble_sort_v2 \n");
+
 	for (int i = 0; i < input_size - 1; i++) {
 		bool swap_flag = false;
+
 		for (int j = 0; j < size-1; j++) {
 			if (is_greater(in[j], in[j+1])) {
 				swap(&in[j], &in[j+1]);
 				swap_flag	=	true;
 			}
 		}
+
 		if (false == swap_flag) {
 			break;
 		}
+
 		--size;
 	}
 }
@@ -55,6 +63,7 @@ void bubble_sort_v3(int *in, int size) {
 	int new = 0;
 
 	printf("\nbubble_sort_v3 \n");
+
 	for (int i = 0; i < size - 1; i++) {
 		for (int j = 0; j < size-1; j++) {
 			if (is_greater(in[j], in[j+1])) {
@@ -62,6 +71,7 @@ void bubble_sort_v3(int *in, int size) {
 				new = j+1;
 			}
 		}
+
 		size = new;
 	}
 }
