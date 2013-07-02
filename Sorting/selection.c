936c48771e0b33eb6fde49a7a1e32ac03d567dba
Neil <neil-lu@graduate.uwa.edu.au>
2013-07-02 19:36:29 +0800

    fixed the functionality of these two selection sort

--- Sorting/selection.c
+++ Sorting/selection.c
@@ -2,29 +2,21 @@
 
 // basic version of selection sort
 void selection_v1(int *in, int size) {
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
-			if (!is_greater(in[i], in[j])) {
-				swap(&in[i], &in[j]);
-			}
-		}
-	}
+	for (int i = 0; i < size - 1; i++) 
+	for (int j = i+1; j < size; j++) 
+		if (is_greater(in[i], in[j])) swap(&in[i], &in[j]);
 }
 
 //improved version introducing index to reduce swaps
 void selection_v2(int *in, int size) {
 	int min, i, j;
 
-	for (int i = 0; i < size - 1; i++) {
+	for (i = 0; i < size - 1; i++) {
 		min = i;
-		for (j = 0; j < size; j++) {
-			if (is_greater(in[i], in[j])) {
-				min = j;
-			}
-		}
+		for (j = i + 1; j < size; j++) 
+			if (is_greater(in[i], in[j])) min = j;
 
-		if (min != i) {
-			swap(&in[i], &in[min]);
-		}
+		if (min != i) swap(&in[i], &in[min]);
+		
 	}
 }
--- Sorting/selection_test.c
+++ Sorting/selection_test.c
@@ -2,8 +2,11 @@
 
 int main(void)
 {
-	int in[30];
+	int in[30], copy_1[30];
 	srand(time(NULL));
+	
+	init_in(in, 30);
+	copy_in(copy_1, in, 30);
 
 	test(in, 30, selection_v1);
 	test(in, 30, selection_v2);
