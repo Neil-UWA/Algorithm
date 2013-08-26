9aa7c678dada1ebb9db1b5f62f02c2bfd0403843
Neil <neil-lu@graduate.uwa.edu.au>
2013-08-26 21:28:52 +0800

    fibonacci number program exercise

---
+++ Fibonacci/fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <time.h>
+
+int fibonacci_number_v0(int n) {
+	if (0 == n || 1 == n) {
+		return n;
+	}
+	return fibonacci_number_v0(n - 1) + fibonacci_number_v0(n - 2);
+}
+
+void test(int (*f)(int), int n) {
+	clock_t start, end;
+
+	start = clock();
+	for (int i = 0; i < n; i++) {
+		printf("%d\n", f(i));
+	}
+	end = clock();
+	printf("%lf secs\n",((double)end - start)/CLOCKS_PER_SEC);
+}
+
+int main(int argc, const char *argv[]) {
+	int n;
+	if (argc != 2) {
+		perror("");
+		exit(EXIT_FAILURE);
+	}
+	n = atoi(argv[1]);
+	test(fibonacci_number_v0, n);
+	return 0;
+}
+
